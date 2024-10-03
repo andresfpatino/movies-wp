8c4a11c51b4d742a2205647636bb6228aa66c721
Andrés Patiño <andres@frontporchsolutions.com>
2024-10-02 21:11:25 -0500

    feat: :sparkles: basic ViewTransitions

---
+++ src/utils/animations.js
@@ -0,0 +1,37 @@
+export const animZoomIn = {
+    old: {
+        name: 'zoomin',
+        duration: '1s',
+        easing: 'ease-linear',
+        direction: 'normal',
+    },
+    new: {
+        name: 'zoomin',
+        duration: '1s',
+        easing: 'ease-linear',
+    },
+};
+
+export const zoomIn = {
+    forwards: animZoomIn,
+    backwards: animZoomIn,
+};
+
+export const animSlideInDown = {
+    old: {
+        name: 'slide-in-down',
+        duration: '2.5s',
+        easing: 'ease-linear',
+        direction: 'normal',
+    },
+    new: {
+        name: 'slide-in-down',
+        duration: '2.5s',
+        easing: 'ease-linear',
+    },
+};
+
+export const slideInDown = {
+    forwards: animSlideInDown,
+    backwards: animSlideInDown,
+};
