deefaa77dafb08dbcd1ea0b726c0f7a88500688d
Andrés Patiño <andres@frontporchsolutions.com>
2024-10-20 15:23:26 -0500

    feat: :test_tube: Change to SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
+//import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   //site: 'https://movies-wp-astro.vercel.app',
   integrations: [react()],
   //output: 'hybrid', // Dejara de existir en la version 5.0
-  // output: 'static', // Para SSG por defecto
-  output: 'server', // Para SSR
-  adapter: vercel(),
+  output: 'static', // Para SSG por defecto
+  //output: 'server', // Para SSR
+  //adapter: vercel(),
 });
