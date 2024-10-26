cb1c9510ecf6f81911c9cd50ac59b58d47544197
Andrés Patiño <andres@frontporchsolutions.com>
2024-10-25 21:53:24 -0500

    test: :test_tube: SSR test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-// import vercel from '@astrojs/vercel/serverless';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
+//import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +9,7 @@ export default defineConfig({
   integrations: [react()],
   ...(process.env.NODE_ENV === 'production' ? [vercel()] : []),
   //output: 'hybrid', // Dejara de existir en la version 5.0
-  output: 'static', // Para SSG por defecto
-  //output: 'server', // Para SSR
+  //output: 'static', // Para SSG por defecto
+  output: 'server', // Para SSR
   adapter: vercel(),
 });
